0cdd67e4eb389b439104ff0227190820e9b04646
nandukalidindi <nandukalidindi@gmail.com>
2018-12-13 22:36:49 -0500

    Changes to the Yeoman generator for answering a question in SO

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,69 +4,69 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the remarkable ' + chalk.red('generator-react-starter-kit-relay-container') + ' generator!'
     ));
 
-    const prompts = [{
+    const tableNamePrompt = [{
       type: 'input',
       name: 'name',
-      message: 'Name of the component'
-    }, {
-      type: 'confirm',
-      name: 'relay',
-      message: 'Do you want to wrap your component in a container(Relay)?',
-      default: false
-    }, {
-      type: 'list',
-      name: 'styleSheets',
-      message: 'How do you want to style your component / container',
-      choices: [
-        'css',
-        'scss'
-      ],
-      default: 'css'
-    }, {
-      type: 'confirm',
-      name: 'lifecycleHooks',
-      message: 'Do you want all the life cycle hooks stubbed in your component?',
-      default: false
-    }, {
-      type: 'input',
-      name: 'location',
-      message: 'Where do you want to place your component?',
-      default: 'src/components/'
+      message: 'The Table Name?'
     }];
 
-    return this.prompt(prompts).then(props => {
-      this.props = props;
-    });
+    const columnPrompts = [{
+      type: 'input',
+      name: 'attributeName',
+      message: 'Define your Schema - ColumnName?',
+      default: 'ID'
+    }, {
+      type: 'input',
+      name: 'attributeType',
+      message: 'Define your Schema - DataType?',
+      default: 'S'
+    }, {
+      type: 'confirm',
+      name: 'repeat',
+      message: 'Do you want to add more columns?',
+      default: 'Y'
+    }]
+
+    this.columns = [];
+
+    const loop = (relevantPrompts) => {
+      return this.prompt(relevantPrompts).then(props => {
+        this.columns.push(props);
+
+        return props.repeat ? loop(columnPrompts) : this.prompt([]);
+
+      })
+    }
+
+    return loop([...tableNamePrompt, ...columnPrompts]);
   }
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('MyComponent.js'),
-      this.destinationPath(this.props.location + this.props.name + '/' + this.props.name + '.js'),
+      this.templatePath('Schema.json'),
+      this.destinationPath('./Schema.json'),
       {
-        name: this.props.name,
-        relay: this.props.relay,
-        styleSheets: this.props.styleSheets,
-        lifecycleHooks: this.props.lifecycleHooks
+        columns: this.columns
       }
     );
 
-    this.fs.copyTpl(
-      this.templatePath('MyComponent.' + this.props.styleSheets),
-      this.destinationPath(this.props.location + this.props.name + '/' + this.props.name + '.' + this.props.styleSheets)
-    );
-
-    this.fs.copyTpl(
-      this.templatePath('package.json'),
-      this.destinationPath(this.props.location + this.props.name + '/' + 'package.json'),
-      {name: this.props.name}
-    );
+    // this.fs.copyTpl(
+    //   this.templatePath('MyComponent.' + this.props.styleSheets),
+    //   this.destinationPath(this.props.location + this.props.name + '/' + this.props.name + '.' + this.props.styleSheets)
+    // );
+    //
+    // this.fs.copyTpl(
+    //   this.templatePath('package.json'),
+    //   this.destinationPath(this.props.location + this.props.name + '/' + 'package.json'),
+    //   {name: this.props.name}
+    // );
   }
 
   // install() {
